8ae3b0f1204f9faf4a67a68e51ff5c2ad579ecfd
Tatarintsev Evgeny <evtatarintsev@ya.ru>
2026-05-03 08:50:18 +0300

    remove sessions today usecase

--- server/src/main/kotlin/org/athletica/crm/routes/HomeRoutes.kt
+++ server/src/main/kotlin/org/athletica/crm/routes/HomeRoutes.kt
@@ -1,11 +1,16 @@
 package org.athletica.crm.routes
 
+import arrow.fx.coroutines.parZip
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toKotlinLocalDate
+import kotlinx.datetime.toLocalDateTime
+import org.athletica.crm.api.schemas.home.TodaySessionItem
 import org.athletica.crm.api.schemas.home.TodaySessionsResponse
 import org.athletica.crm.domain.groups.Groups
 import org.athletica.crm.domain.hall.Halls
 import org.athletica.crm.domain.sessions.Sessions
 import org.athletica.crm.storage.Database
-import org.athletica.crm.usecases.home.todaySessions
+import kotlin.time.Clock
 
 context(db: Database)
 fun RouteWithContext.homeRoutes(
@@ -15,7 +20,38 @@ fun RouteWithContext.homeRoutes(
 ) {
     route("/home") {
         get<TodaySessionsResponse>("/today-sessions") {
-            todaySessions(db, groups, sessions, halls)
+            val today = Clock.System
+                .now()
+                .toLocalDateTime(TimeZone.currentSystemDefault())
+                .date
+
+            db.transaction {
+                val items = parZip(
+                    { sessions.listAll(today, today) },
+                    { halls.list().associateBy { it.id } },
+                    { groups.list().associate { it.id to it.name } }
+                ) { sessions, hallMap, groupNames ->
+                    sessions
+                        .filter { it.status == "scheduled" }
+                        .map { session ->
+                            val hallName = hallMap[session.hallId]?.name ?: "Не указан"
+                            val groupName = groupNames[session.groupId] ?: ""
+                            TodaySessionItem(
+                                sessionId = session.id,
+                                groupName = groupName,
+                                startTime = session.startTime,
+                                endTime = session.endTime,
+                                hallName = hallName,
+                            )
+                        }
+                        .sortedBy { it.startTime }
+                }
+
+
+
+
+                TodaySessionsResponse(date = today, sessions = items)
+            }
         }
     }
 }
